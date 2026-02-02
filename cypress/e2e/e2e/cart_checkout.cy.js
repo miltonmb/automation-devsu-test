f3494573c6f69dc90d662f51132397b2d3fc80da
miltonmb <miltonmb@unitec.edu>
2026-02-02 14:17:40 -0600

    Primer objeto agregado al carrito

--- cypress/e2e/e2e/cart_checkout.cy.js
+++ cypress/e2e/e2e/cart_checkout.cy.js
@@ -1,6 +1,10 @@
 describe("Flujo compra como invitado", ()=>{
     it('compra exitosa como invitado', () => {
         cy.visit('http://opencart.abstracta.us/')
+
+        cy.contains('Canon EOS 5D').click();
+        cy.get('select').select('15');
+        cy.get('#button-cart').click();
         
     });
 })
