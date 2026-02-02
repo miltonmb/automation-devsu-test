94ca59678b0e0239bd8bec4315971af788b3e842
miltonmb <miltonmb@unitec.edu>
2026-02-02 16:56:29 -0600

    flujo de compra exitosa como invitado funcionando correctamente

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    //baseUrl: 'https://opencart.abstracta.us',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/e2e/cart_checkout.cy.js
+++ cypress/e2e/e2e/cart_checkout.cy.js
@@ -1,4 +1,4 @@
-describe("Flujo compra como invitado", ()=>{
+describe("Flujo compra como invitado", () => {
     it('compra exitosa como invitado', () => {
         cy.visit('http://opencart.abstracta.us/')
 
@@ -13,6 +13,33 @@ describe("Flujo compra como invitado", ()=>{
         cy.contains('Phones & PDAs').click();
         cy.contains('HTC Touch HD').click();
         cy.get('#button-cart').click();
-        
+
+        //ver el carrito y comprar como guest
+        cy.contains('Shopping Cart').click();
+        cy.contains('Checkout').click();
+        cy.origin('https://opencart.abstracta.us', () => {
+            cy.get('input[value="guest"]').check();
+            cy.get('#button-account').click();
+            cy.get('#input-payment-firstname').type('Milton');
+            cy.get('#input-payment-lastname').type('Martinez');
+            cy.get('#input-payment-email').type('test@test.com');
+            cy.get('#input-payment-telephone').type('33159971');
+            cy.get('#input-payment-address-1').type('Residencial de prueba 1234');
+            cy.get('#input-payment-city').type('Tegucigalpa');
+            cy.get('#input-payment-postcode').type('11101');
+            cy.get('#input-payment-country').select('95');
+            cy.get('#input-payment-zone').select('1399');
+            cy.get('#button-guest').click();
+            cy.get('#button-shipping-method').click();
+            cy.get('input[value="cod"]').check();
+            cy.get('input[name="agree"]').check();
+            cy.get('#button-payment-method').click();
+            cy.get('#button-confirm').click();
+        });
+        cy.contains('Your order has been placed!', { timeout: 10000 }).should('be.visible');
+
+
+
     });
-})
+});
+
